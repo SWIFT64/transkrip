3a8489e54ed24c6ccb8f9cd7ecb7c1492ef6794f
HeroBuxx <me@herobuxx.me>
2023-11-25 17:15:13 +0700

    transkrip: Split template into two pages
    Co-authored-by: Signed-off-by: JPratama7 <josepratama080@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -9,8 +10,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strconv"
+
+	"github.com/chromedp/cdproto/cdp"
 
 	"github.com/aiteung/athelper/fiber"
 	"github.com/chromedp/cdproto/page"
@@ -19,8 +21,8 @@ import (
 
 // Config struct to hold configuration values
 type Config struct {
-	APIURL      string `json:"apiURL"`
-	LoginToken  string `json:"loginToken"`
+	APIURL     string `json:"apiURL"`
+	LoginToken string `json:"loginToken"`
 }
 
 type Data struct {
@@ -49,6 +51,38 @@ type Data struct {
 	NikDekan          int           `json:"nik_dekan"`
 }
 
+type PageOnePDF struct {
+	NamaMhs        string    `json:"nama_mhs"`
+	NomorIndukMhs  int       `json:"nomor_induk_mhs"`
+	TtlMhs         string    `json:"ttl_mhs"`
+	TtlMhsEng      string    `json:"ttl_mhs_eng"`
+	TahunMasukMhs  string    `json:"tahun_masuk_mhs"`
+	FakultasMhs    string    `json:"fakultas_mhs"`
+	FakultasMhsEng string    `json:"fakultas_mhs_eng"`
+	ProdiMhs       string    `json:"prodi_mhs"`
+	ProdiMhsEng    string    `json:"prodi_mhs_eng"`
+	NoTranskrip    string    `json:"no_transkrip"`
+	Subjects       []Subject `json:"subjects"`
+}
+type PageTwoPDF struct {
+	CreditsTotal      int           `json:"credits_total"`
+	GradeTotal        float64       `json:"grade_total"`
+	GraduationDate    string        `json:"graduation_date"`
+	GraduationDateEng string        `json:"graduation_date_eng"`
+	Subjects          []Subject     `json:"subjects"`
+	PredikatMhs       string        `json:"predikat_mhs"`
+	PredikatMhsEng    string        `json:"predikat_mhs_eng"`
+	JudulSkriptsi     JudulSkriptsi `json:"judul_skriptsi"`
+	TempatTerbit      string        `json:"tempat_terbit"`
+	TanggalTerbit     string        `json:"tanggal_terbit"`
+	FakultasMhs       string        `json:"fakultas_mhs"`
+
+	TanggalTerbitEng string `json:"tanggal_terbit_eng"`
+	NamaDekan        string `json:"nama_dekan"`
+	NoTranskrip      string `json:"no_transkrip"`
+	NikDekan         int    `json:"nik_dekan"`
+}
+
 type Subject struct {
 	Index       int    `json:"index"`
 	Subjname    string `json:"subjname"`
@@ -69,7 +103,7 @@ func main() {
 		log.Println("Error reading configuration:", err)
 		return
 	}
-		// Replace the mock API URL with the Ulbi API URL
+	// Replace the mock API URL with the Ulbi API URL
 
 	apiURL := config.APIURL
 
@@ -99,7 +133,7 @@ func main() {
 	}
 
 	// Parse JSON data into a single Data struct
-	fmt.Printf("%s\n\n", body)
+	//fmt.Printf("%s\n\n", body)
 	var raw fiber.ReturnData[Data]
 	err = json.Unmarshal(body, &raw)
 	if err != nil {
@@ -119,66 +153,186 @@ func main() {
 		student.Subjects[i].Index = i + 1
 	}
 
+	pdfPageOne := PageOnePDF{
+		NamaMhs:        student.NamaMhs,
+		NomorIndukMhs:  student.NomorIndukMhs,
+		TtlMhs:         student.TtlMhs,
+		TtlMhsEng:      student.TtlMhsEng,
+		TahunMasukMhs:  student.TahunMasukMhs,
+		FakultasMhs:    student.FakultasMhs,
+		FakultasMhsEng: student.FakultasMhsEng,
+		ProdiMhs:       student.ProdiMhs,
+		ProdiMhsEng:    student.ProdiMhsEng,
+		NoTranskrip:    student.NoTranskrip,
+		Subjects:       student.Subjects[:42],
+	}
+
+	pdfPageTwo := PageTwoPDF{
+		CreditsTotal:      student.CreditsTotal,
+		GradeTotal:        student.GradeTotal,
+		GraduationDate:    student.GraduationDate,
+		GraduationDateEng: student.GraduationDateEng,
+		Subjects:          student.Subjects[38:],
+		PredikatMhs:       student.PredikatMhs,
+		PredikatMhsEng:    student.PredikatMhsEng,
+		JudulSkriptsi:     student.JudulSkriptsi,
+		TempatTerbit:      student.TempatTerbit,
+		TanggalTerbit:     student.TanggalTerbit,
+		TanggalTerbitEng:  student.TanggalTerbitEng,
+		NamaDekan:         student.NamaDekan,
+		NoTranskrip:       student.NoTranskrip,
+		FakultasMhs:       student.FakultasMhs,
+		NikDekan:          student.NikDekan,
+	}
+
 	// Read the HTML template from the file
-	templateFile := "template/transcript.html"
-	htmlTemplate, err := ioutil.ReadFile(templateFile)
+	templateFile1 := "template/page_1.html"
+	templateFile2 := "template/page_2.html"
+	htmlTemplate1, err := os.ReadFile(templateFile1)
+	if err != nil {
+		panic(err)
+	}
+
+	htmlTemplate2, err := os.ReadFile(templateFile2)
 	if err != nil {
 		panic(err)
 	}
 
 	// Parse the HTML template
-	tmpl, err := template.New("transcript").Parse(string(htmlTemplate))
+	tmpl1, err := template.New("page_1").Parse(string(htmlTemplate1))
+	if err != nil {
+		panic(err)
+	}
+	// Parse the HTML template
+	tmpl2, err := template.New("page_2").Parse(string(htmlTemplate2))
 	if err != nil {
 		panic(err)
 	}
 
 	// Create a new file to write the output
-	outputFileName := "output_" + strconv.Itoa(student.NomorIndukMhs) + ".html"
-	outputFile, err := os.Create(outputFileName)
-	if err != nil {
-		log.Println("Error creating output file:", err)
-		return
-	}
-	defer outputFile.Close()
+	//outputFileName := "output_" + strconv.Itoa(student.NomorIndukMhs) + ".html"
+	//outputFile, err := os.Create(outputFileName)
+	//if err != nil {
+	//	log.Println("Error creating output file:", err)
+	//	return
+	//}
+	//defer outputFile.Close()
+	//
+	//// Create a new file to write the output
+	//outputFileName2 := "output2_" + strconv.Itoa(student.NomorIndukMhs) + ".html"
+	//outputFile2, err := os.Create(outputFileName2)
+	//if err != nil {
+	//	log.Println("Error creating output file:", err)
+	//	return
+	//}
+	//defer outputFile.Close()
+
+	pdf1 := new(bytes.Buffer)
+	pdf2 := new(bytes.Buffer)
 
 	// Execute the template and write the HTML content to the file
-	err = tmpl.Execute(outputFile, student)
+	err = tmpl1.Execute(pdf1, pdfPageOne)
+	if err != nil {
+		log.Println("Error executing template:", err)
+		return
+	}
+	// Execute the template and write the HTML content to the file
+	err = tmpl2.Execute(pdf2, pdfPageTwo)
 	if err != nil {
 		log.Println("Error executing template:", err)
 		return
 	}
 
 	// Get the absolute path of the HTML file
-	absPath, err := filepath.Abs(outputFile.Name())
-	if err != nil {
-		log.Println("Error getting absolute path:", err)
-		return
-	}
-
-	// Print the file path for debugging
-	log.Println("HTML file path:", absPath)
+	//absPath, err := filepath.Abs(outputFile.Name())
+	//if err != nil {
+	//	log.Println("Error getting absolute path:", err)
+	//	return
+	//}
+	//
+	//// Print the file path for debugging
+	//log.Println("HTML file path:", absPath)
 
 	// Create a context
-	ctx, cancel := chromedp.NewContext(context.Background())
+	opt := append(chromedp.DefaultExecAllocatorOptions[:], chromedp.ExecPath("/usr/bin/chromium"))
+
+	allocatorCtx, allocatorCancel := chromedp.NewExecAllocator(
+		context.Background(),
+		opt...,
+	)
+	defer allocatorCancel()
+
+	ctx, cancel := chromedp.NewContext(allocatorCtx)
 	defer cancel()
 
 	// Open the HTML file using chromedp
-	err = chromedp.Run(ctx,
-		chromedp.Navigate("file://"+absPath),
-		chromedp.WaitReady("body"),
-	)
-	if err != nil {
-		log.Println("Error navigating to HTML file:", err)
-		return
-	}
+	//err = chromedp.Run(ctx,
+	//	chromedp.Navigate("file://"+absPath),
+	//	chromedp.WaitReady("body"),
+	//)
+	//if err != nil {
+	//	log.Println("Error navigating to HTML file:", err)
+	//	return
+	//}
 
 	// Print the page to PDF
 	var pdfData []byte
+	var pdfData2 []byte
+	//err = chromedp.Run(ctx,
+	//	chromedp.ActionFunc(func(ctx context.Context) error {
+	//		var err error
+	//		pdfData, _, err = page.PrintToPDF().Do(ctx)
+	//		return err
+	//	}),
+	//)
+
+	// Generate Page 1
 	err = chromedp.Run(ctx,
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var err error
-			pdfData, _, err = page.PrintToPDF().Do(ctx)
-			return err
+		chromedp.Navigate("about:blank"),
+		chromedp.ActionFunc(func(c context.Context) (err error) {
+			chromepCtx := chromedp.FromContext(c)
+			executor := cdp.WithExecutor(c, chromepCtx.Target)
+			frame, err := page.GetFrameTree().Do(executor)
+			if err != nil {
+				fmt.Printf("error frame tree %+v \n ", err)
+				return
+			}
+
+			err = page.SetDocumentContent(frame.Frame.ID, pdf1.String()).Do(executor)
+			return
+		}),
+		chromedp.WaitReady("body"),
+		chromedp.ActionFunc(func(c context.Context) (err error) {
+			chromepCtx := chromedp.FromContext(c)
+			pdfData, _, err = page.PrintToPDF().Do(cdp.WithExecutor(c, chromepCtx.Target))
+			return
+		}),
+	)
+	if err != nil {
+		log.Println("Error printing to PDF:", err)
+		return
+	}
+
+	// Generate Page 2
+	err = chromedp.Run(ctx,
+		chromedp.Navigate("about:blank"),
+		chromedp.ActionFunc(func(c context.Context) (err error) {
+			chromepCtx := chromedp.FromContext(c)
+			executor := cdp.WithExecutor(c, chromepCtx.Target)
+			frame, err := page.GetFrameTree().Do(executor)
+			if err != nil {
+				fmt.Printf("error frame tree %+v \n ", err)
+				return
+			}
+
+			err = page.SetDocumentContent(frame.Frame.ID, pdf2.String()).Do(executor)
+			return
+		}),
+		chromedp.WaitReady("body"),
+		chromedp.ActionFunc(func(c context.Context) (err error) {
+			chromepCtx := chromedp.FromContext(c)
+			pdfData2, _, err = page.PrintToPDF().Do(cdp.WithExecutor(c, chromepCtx.Target))
+			return
 		}),
 	)
 	if err != nil {
@@ -188,7 +342,13 @@ func main() {
 
 	// Write the PDF data to a file
 	pdfFileName := "output_" + strconv.Itoa(student.NomorIndukMhs) + ".pdf"
-	err = ioutil.WriteFile(pdfFileName, pdfData, 0644)
+	err = os.WriteFile(pdfFileName, pdfData, 0644)
+	if err != nil {
+		log.Println("Error writing PDF file:", err)
+		return
+	}
+	pdfFileName2 := "output2_" + strconv.Itoa(student.NomorIndukMhs) + ".pdf"
+	err = os.WriteFile(pdfFileName2, pdfData2, 0644)
 	if err != nil {
 		log.Println("Error writing PDF file:", err)
 		return
